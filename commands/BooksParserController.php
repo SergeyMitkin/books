66f8dc9a25d1831bd7e716f101cd8e1c91addca0
SergeyMitkin <sergeymitkin@gmail.com>
2025-03-12 02:42:44 +0300

    url с книгами вынеснен в отдельное свойство

--- commands/BooksParserController.php
+++ commands/BooksParserController.php
@@ -8,11 +8,12 @@ use yii\helpers\Json;
 
 class BooksParserController extends Controller
 {
+    private $books_url = 'https://gitlab.com/prog-positron/test-app-vacancy/-/raw/master/books.json';
     /**
      * Парсинг книг из файла
      */
     public function actionIndex() {
-        $data = file_get_contents('https://gitlab.com/prog-positron/test-app-vacancy/-/raw/master/books.json');
+        $data = file_get_contents($this->books_url);
         $books_arr = Json::decode($data, true);
         $books_model = new Books();
 
