293585c31bea38819dba06f18c87bb1e2bbd42c9
SergeyMitkin <sergeymitkin@gmail.com>
2025-03-12 04:43:50 +0300

    Удалён тестовый метод

--- modules/admin/controllers/SiteController.php
+++ modules/admin/controllers/SiteController.php
@@ -18,19 +18,6 @@ class SiteController extends Controller
         return $this->render('index');
     }
 
-    public function actionTest()
-    {
-        $data = file_get_contents('https://gitlab.com/prog-positron/test-app-vacancy/-/raw/master/books.json');
-        $books_arr = Json::decode($data, true);
-
-        // --- ОТЛАДКА НАЧАЛО
-        echo '<pre>';
-        var_dump($books_arr);
-        echo'</pre>';
-        die;
-        // --- Отладка конец
-    }
-
     public function actionLogin()
     {
         $this->layout = 'main-login';
